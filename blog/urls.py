17c66179b7be98b267e06400349eae6f3e993040
mlmd <ivantkachuk44@gmail.com>
2018-06-15 09:59:56 +0400

    Changet for use href

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+	url(r'^$', views.index1, name='index1'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+	
+def index1(request):
+    return render(request, 'blog/index1.html', {})
